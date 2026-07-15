f60a1684db36022264f493e2434ebfd6496a88b7
RfadnjdExt <raficanggih@gmail.com>
2026-07-16 03:57:21 +0700

    style: tambah ikon pada tombol logout sidebar petani

--- resources/views/layouts/petani.blade.php
+++ resources/views/layouts/petani.blade.php
@@ -21,7 +21,7 @@
       <hr>
       <a href="{{ route('home') }}">← Lihat Situs</a>
       <a href="{{ route('logout') }}"
-         onclick="event.preventDefault(); document.getElementById('petani-logout-form').submit();">Logout</a>
+         onclick="event.preventDefault(); document.getElementById('petani-logout-form').submit();"><svg class="icon-inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg> Logout</a>
       <form id="petani-logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
         @csrf
       </form>
